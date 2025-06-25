6ba68227761ba49eca939366826dc9cb87f8b88d
Alexander01998 <contact.wurstimperium@gmail.com>
2025-06-25 17:02:45 +0200

    Update snapshot thread

--- scripts/announce_snapshot_ports.py
+++ scripts/announce_snapshot_ports.py
@@ -2,7 +2,7 @@ import util
 from argparse import ArgumentParser
 from pathlib import Path
 
-current_snapshot_discussion = 1059
+current_snapshot_discussion = 1190
 
 
 def get_link(mod: str, branch: str) -> str:
